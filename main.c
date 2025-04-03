23a1669a2b8516832256cae0002652bcbd10bf80
Benjamin J. Thompson <bjt@rabidquill.com>
2025-04-03 15:50:32 -0400

    added load_mem_texture(), added destroy_and_quit(), implemented globals to set screen size to HD, made use of both rocket images and implemented rocket lighting when the thruster is on.

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <math.h>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <stdbool.h>
+
 #include "includes/rockets.h"
+
+int WIN_WIDTH = 1280;
+int WIN_HEIGHT = 720;
+
+void destroy_and_quit(SDL_Window *window, SDL_Renderer *renderer) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+SDL_Texture* load_mem_texture(SDL_Renderer* renderer, const unsigned char* img, const unsigned int len) {
+    // Load image
+    SDL_RWops* rw = SDL_RWFromConstMem(img, (int) len);
+    if (rw == NULL) {
+        printf("SDL_RWFromConstMem failed: %s\n", SDL_GetError());
+        return NULL;
+    }
+
+    SDL_Surface* surface = IMG_Load_RW(rw, 1);
+    if (surface == NULL) {
+        printf("IMG_Load_RW failed: %s\n", SDL_GetError());
+        return NULL;
+    }
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    if (texture == NULL) {
+        printf("SDL_CreateTextureFromSurface failed: %s\n", SDL_GetError());
+        return NULL;
+    }
+
+    return texture;
+}
 
 int main(void) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -13,8 +49,8 @@ int main(void) {
         "Do The Flop",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        WIN_WIDTH,
+        WIN_HEIGHT,
         SDL_WINDOW_SHOWN);
 
     if (window == NULL) {
@@ -31,52 +67,33 @@ int main(void) {
         return 1;
     }
 
-    // Load image
-    SDL_RWops* rw = SDL_RWFromConstMem(rocket, rocket_len);
-    if (rw == NULL) {
-        printf("SDL_RWFromConstMem failed: %s\n", SDL_GetError());
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
+    SDL_Texture* rocket_unlit = load_mem_texture(renderer, rocket, rocket_len);
+    SDL_Texture* rocket_islit = load_mem_texture(renderer, rocket_lit, rocket_lit_len);
 
-    SDL_Surface* surface = IMG_Load_RW(rw, 1);
-    if (surface == NULL) {
-        printf("IMG_Load_RW failed: %s\n", SDL_GetError());
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-    if (texture == NULL) {
-        printf("SDL_CreateTextureFromSurface failed: %s\n", SDL_GetError());
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
+    SDL_Texture* rocket_sprite = rocket_unlit;
 
     int texWidth, texHeight;
-    if (SDL_QueryTexture(texture, NULL, NULL,
+    if (SDL_QueryTexture(rocket_unlit, NULL, NULL,
                         &texWidth, &texHeight) != 0) {
         printf("SDL_QueryTexture failed: %s\n", SDL_GetError());
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+        destroy_and_quit(window, renderer);
         return 1;
                         }
 
+    SDL_Rect bgRect;
+    bgRect.x = 0;
+    bgRect.y = 0;
+    bgRect.w = WIN_WIDTH;
+    bgRect.h = WIN_HEIGHT;
+
     SDL_Rect rocketRect;
-    rocketRect.x = (800 - texWidth) / 2;
-    rocketRect.y = (600 - texHeight) / 2;
+    rocketRect.x = (WIN_WIDTH - texWidth) / 2;
+    rocketRect.y = (WIN_HEIGHT - texHeight) / 2;
     rocketRect.w = texWidth;
     rocketRect.h = texHeight;
-
-    int vy = 0;
+    float g = 0.4f;
+    float vy = 0;
+    bool thruster_on = false;
 
     // Event Loop
     int running = 1;
@@ -87,18 +104,32 @@ int main(void) {
                 running = 0;
             }
             if (event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_UP) {
-                vy -= 1;
+
+                rocket_sprite = rocket_islit;
+                thruster_on = true;
             }
-            if (event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_DOWN) {
-                vy += 1;
+            if (event.type == SDL_KEYUP && event.key.keysym.scancode == SDL_SCANCODE_UP) {
+                rocket_sprite = rocket_unlit;
+                thruster_on = false;
             }
         }
 
+        if (thruster_on) {
+            vy -= 1;
+        }
+        vy += g;
+
         SDL_RenderClear(renderer);
+        rocketRect.y += (int) round(vy);
+        if (rocketRect.y >= (WIN_HEIGHT - rocketRect.h)) {
+            rocketRect.y = (WIN_HEIGHT - rocketRect.h);
+            vy = 0;
+        }
 
-        rocketRect.y += vy;
+        SDL_SetRenderDrawColor(renderer, 64, 100, 128, 255);
+        SDL_RenderFillRect(renderer, NULL);
 
-        if (SDL_RenderCopy(renderer, texture, NULL, &rocketRect) != 0) {
+        if (SDL_RenderCopy(renderer, rocket_sprite, NULL, &rocketRect) != 0) {
             printf("SDL_RenderCopy failed: %s\n", SDL_GetError());
             running = 0;
             break;
@@ -108,9 +139,9 @@ int main(void) {
         SDL_Delay(34);
     }
 
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    SDL_DestroyTexture(rocket_sprite);
+    SDL_DestroyTexture(rocket_islit);
+    SDL_DestroyTexture(rocket_unlit);
+    destroy_and_quit(window, renderer);
     return 0;
 }
